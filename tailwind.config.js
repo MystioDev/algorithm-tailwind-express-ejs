016de0feb57e7ac36b513ad1ed940383ab0c9e46
VaradiMarcell <mystio.developer@gmail.com>
2024-01-22 18:15:55 +0100

    Hero Section V2 - Blob Background Image

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,12 @@ module.exports = {
   content: ["./views/**/*.ejs"],
   mode: "jit",
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "hero-pattern": "url('/image/blob_bg.png')",
+        "footer-texture": "url('/img/footer-texture.png')",
+      },
+    },
   },
   plugins: [],
 };
